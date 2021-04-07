2483f52ec4959d36da0970817b10affd5a409ffa
742359579@qq.com <mf1923078>
2021-04-07 20:53:17 +0800

    堆排序和冒泡排序

---
+++ sortPractice/bubbleSort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void bubbleSort(vector<int>& nums) {
+
+	int n=nums.size();
+	for(int i=0;i<n;++i) {
+		for(int j=n-1;j>i;--j) {
+			if (nums[j]<nums[j-1])
+				swap(nums[j],nums[j-1]);
+
+		}
+	}
+
+}
+void test01()
+{
+	vector<int> v{2,10,7,3,6,4,9};
+	bubbleSort(v);
+	for_each(v.begin(),v.end(),[](int x){
+			cout<<x<<" ";});
+}
+int main()
+{
+	test01();
+	return 0;
+}
---
+++ sortPractice/heapSort.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+void adjust_heap(vector<int>&nums,int idx,int len) {
+	int child = 2*idx+1;
+	while (child<len) {
+
+		if (child+1<len && nums[child+1]>nums[child])
+			++child;
+		if (nums[idx]>nums[child])
+			break;
+		swap(nums[idx],nums[child]);
+		idx = child;
+		child=child*2+1;
+
+	}	
+
+
+}
+void make_heap(vector<int> & nums) {
+
+	int n = nums.size();
+	for(int i=n/2-1;i>=0;--i) {
+		adjust_heap(nums,i,n);
+	}
+
+}
+void heap_sort(vector<int> &nums)
+{
+
+	make_heap(nums);
+	int n = nums.size();
+	for(int i=n-1;i>=0;--i) {
+		swap(nums[i],nums[0]);
+		adjust_heap(nums,0,i);
+
+	}
+
+
+}
+void test01()
+{
+	vector<int> v{2,6,1,9,3,10,4};
+	heap_sort(v);
+	for_each(v.begin(),v.end(),[](int x){
+			cout<<x<<" ";});
+
+}
+int main()
+{
+	test01();
+	return 0;
+}
+
